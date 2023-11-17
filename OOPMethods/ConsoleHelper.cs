7d552dc9b88d16f8840576cddeba9c638ba566ca
briansomogyi <brian.somogyi@emanuel.ro>
2023-11-17 08:44:23 +0200

    DRY technique for method overloading

--- OOPMethods/ConsoleHelper.cs
+++ OOPMethods/ConsoleHelper.cs
@@ -10,20 +10,26 @@ namespace OOPMethods
     {
         public static void PrintMessage(string message)
         {
-            Console.WriteLine(message);
+            PrintMessage(string.Empty, message, 1);
         }
 
         public static void PrintMessage(string label, string message) 
         {
-            if (!string.IsNullOrEmpty(label)) 
-            {
-                Console.WriteLine($"{label}: ");
-            }
-            Console.WriteLine($"{message}");
+            PrintMessage(label, message, 1);
         }
 
         public static void PrintMessage(string message, int count)
         {
+            PrintMessage(string.Empty, message, count);
+        }
+
+        public static void PrintMessage(string label, string message, int count)
+        {
+            if (!string.IsNullOrEmpty(label))
+            {
+                Console.Write($"{label}: ");
+            }
+            
             for (int i = 0; i < count; i++)
             {
                 Console.WriteLine(message);
