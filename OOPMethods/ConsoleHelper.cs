922c45dd0de08190302474c8fb11cfccaf83e32c
briansomogyi <brian.somogyi@emanuel.ro>
2023-11-17 08:34:07 +0200

    Method overloading usage

---
+++ OOPMethods/ConsoleHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPMethods
+{
+    internal static class ConsoleHelper
+    {
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void PrintMessage(string label, string message) 
+        {
+            if (!string.IsNullOrEmpty(label)) 
+            {
+                Console.WriteLine($"{label}: ");
+            }
+            Console.WriteLine($"{message}");
+        }
+
+        public static void PrintMessage(string message, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ OOPMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.PrintMessage("test");
+            
+            ConsoleHelper.PrintMessage("lable", "test");
+            
+            ConsoleHelper.PrintMessage("test", 3);
+
+        }
+    }
+}
